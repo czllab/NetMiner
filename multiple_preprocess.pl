c04a71492ebe8f12a6353a64f3e0d9fedb1dfdc1
hyu <huayu@genetics.ac.cn>
2016-08-26 11:04:09 +0800

    Add files via upload

--- multiple_preprocess.pl
+++ multiple_preprocess.pl
@@ -149,47 +149,46 @@ EOF
 		chomp $in_p1;
 		$in_p2 = `find $$outputpath/$$rawfastqpath/$$runHash{$run} -name "$runid\_2.fastq.gz"`;
 		chomp $in_p2;
-		if ($$trimmomatic){
-			if(!-e "$$outputpath/$$cleanpath/$$runHash{$run}/$runid".".clean_PE_1.fastq.gz" && !-e "$$outputpath/$$cleanpath/$$runHash{$run}/$runid".".clean_SE_1.fastq.gz"){
-				if(-e $in_p1){
-					if(-e $in_p2){
-						my $minLen = int (($$readLength{$runid}*7/20)+0.5);
-						$type="PE";
-						$out_p1="$$outputpath/$$cleanpath/$$runHash{$run}/$runid".".clean_PE_1.fastq.gz";
-						$out_p2="$$outputpath/$$cleanpath/$$runHash{$run}/$runid".".clean_PE_2.fastq.gz";
-						$out_up1="$$outputpath/$$cleanpath/$$runHash{$run}/$runid".".clean_UP_1.fastq.gz";
-						$out_up2="$$outputpath/$$cleanpath/$$runHash{$run}/$runid".".clean_UP_2.fastq.gz";
-						if ($$machine{$runid}=~/Illumina HiSeq/){
-							$step="ILLUMINACLIP:/public/software/Trimmomatic-0.32/adapters/TruSeq3-PE.fa:2:30:3 LEADING:2 TRAILING:2 SLIDINGWINDOW:3:2 MINLEN:$minLen TOPHRED33";
-						}else{
-							$step="ILLUMINACLIP:/public/software/Trimmomatic-0.32/adapters/TruSeq2-PE.fa:2:30:3 LEADING:2 TRAILING:2 SLIDINGWINDOW:3:2 MINLEN:$minLen TOPHRED33";
-						}		
+		if(!-e "$$outputpath/$$cleanpath/$$runHash{$run}/$runid".".clean_PE_1.fastq.gz" && !-e "$$outputpath/$$cleanpath/$$runHash{$run}/$runid".".clean_SE_1.fastq.gz"){
+			if(-e $in_p1){
+				if(-e $in_p2){
+					my $minLen = int (($$readLength{$runid}*7/20)+0.5);
+					$type="PE";
+					$out_p1="$$outputpath/$$cleanpath/$$runHash{$run}/$runid".".clean_PE_1.fastq.gz";
+					$out_p2="$$outputpath/$$cleanpath/$$runHash{$run}/$runid".".clean_PE_2.fastq.gz";
+					$out_up1="$$outputpath/$$cleanpath/$$runHash{$run}/$runid".".clean_UP_1.fastq.gz";
+					$out_up2="$$outputpath/$$cleanpath/$$runHash{$run}/$runid".".clean_UP_2.fastq.gz";
+					if ($$machine{$runid}=~/Illumina HiSeq/){
+						$step="ILLUMINACLIP:/public/software/Trimmomatic-0.32/adapters/TruSeq3-PE.fa:2:30:3 LEADING:2 TRAILING:2 SLIDINGWINDOW:3:2 MINLEN:$minLen TOPHRED33";
 					}else{
-						my $minLen = int (($$readLength{$runid}*7/10)+0.5);
-						$type="SE"; 
-						$out_p1="$$outputpath/$$cleanpath/$$runHash{$run}/$runid".".clean_SE_1.fastq.gz";
-						if ($$machine{$runid}=~/Illumina HiSeq/){
-							$step="ILLUMINACLIP:/public/software/Trimmomatic-0.32/adapters/TruSeq3-SE.fa:2:30:3 LEADING:2 TRAILING:2 SLIDINGWINDOW:3:2 MINLEN:$minLen TOPHRED33";
-						}else{
-							$step="ILLUMINACLIP:/public/software/Trimmomatic-0.32/adapters/TruSeq2-SE.fa:2:30:3 LEADING:2 TRAILING:2 SLIDINGWINDOW:3:2 MINLEN:$minLen TOPHRED33";
-						}
+						$step="ILLUMINACLIP:/public/software/Trimmomatic-0.32/adapters/TruSeq2-PE.fa:2:30:3 LEADING:2 TRAILING:2 SLIDINGWINDOW:3:2 MINLEN:$minLen TOPHRED33";
+					}		
+				}else{
+					my $minLen = int (($$readLength{$runid}*7/10)+0.5);
+					$type="SE"; 
+					$out_p1="$$outputpath/$$cleanpath/$$runHash{$run}/$runid".".clean_SE_1.fastq.gz";
+					if ($$machine{$runid}=~/Illumina HiSeq/){
+						$step="ILLUMINACLIP:/public/software/Trimmomatic-0.32/adapters/TruSeq3-SE.fa:2:30:3 LEADING:2 TRAILING:2 SLIDINGWINDOW:3:2 MINLEN:$minLen TOPHRED33";
+					}else{
+						$step="ILLUMINACLIP:/public/software/Trimmomatic-0.32/adapters/TruSeq2-SE.fa:2:30:3 LEADING:2 TRAILING:2 SLIDINGWINDOW:3:2 MINLEN:$minLen TOPHRED33";
 					}
-				}else{
-					print "couldn't find the fastq file of $$outputpath/$$rawfastqpath/$$runHash{$run}/${runid}_1.fastq.gz\n";
 				}
-				if(-e $in_p1 && $in_p2 eq ""){
-					$operators = "$in_p1 $out_p1 $step";
-				}elsif(-e $in_p1 && -e $in_p2){
-					$operators = "$in_p1 $in_p2 $out_p1 $out_up1 $out_p2 $out_up2 $step"; 
-				}else{
-					$operators = "";
-				}
+			}else{
+				print "couldn't find the fastq file of $$outputpath/$$rawfastqpath/$$runHash{$run}/${runid}_1.fastq.gz\n";
+			}
+			if(-e $in_p1 && $in_p2 eq ""){
+				$operators = "$in_p1 $out_p1 $step";
+			}elsif(-e $in_p1 && -e $in_p2){
+				$operators = "$in_p1 $in_p2 $out_p1 $out_up1 $out_p2 $out_up2 $step"; 
 			}else{
 				$operators = "";
 			}
-			if($operators ne ""){
-				open OUT,">$$outputpath/$$pbspath/$runHash{$run}/$jobname\_$runid".".pbs";
-				print OUT <<EOF;
+		}else{
+			$operators = "";
+		}
+		if($operators ne ""){
+			open OUT,">$$outputpath/$$pbspath/$runHash{$run}/$jobname\_$runid".".pbs";
+			print OUT <<EOF;
 #PBS -N $jobname\_$runid
 #PBS -j oe
 #PBS -l nodes=1:ppn=$$threads
@@ -201,23 +200,23 @@ cd \$PBS_O_WORKDIR
 date   
 /public/software/jdk1.7.0_45/bin/java -jar /public/software/Trimmomatic-0.32/trimmomatic-0.32.jar $type -threads $$threads $operators
 for file in `find $$outputpath/$$cleanpath/$$runHash{$run} -name "${runid}*clean*.fastq.gz"`
-	do 
-		sample=\${file%.fastq.gz}
-		sample=\${sample##\/*\/}
-		gunzip -c \$file | fastq_quality_filter -v -q $QUAL -p $PERCENT -z -Q 33 -o $$outputpath/$$cleanpath/$$runHash{$run}/\$sample.fastx.fastq.gz
-	done
+do 
+	sample=\${file%.fastq.gz}
+	sample=\${sample##\/*\/}
+	gunzip -c \$file | fastq_quality_filter -v -q $QUAL -p $PERCENT -z -Q 33 -o $$outputpath/$$cleanpath/$$runHash{$run}/\$sample.fastx.fastq.gz
+done
 fastqc -o $$outputpath/$$fastqcpath/$$runHash{$run} -t $$threads --extract -f fastq $$outputpath/$$cleanpath/$$runHash{$run}/$runid*.fastx.fastq.gz
 date
 EOF
-				close OUT;
-				$marksfastqfilter{$runid} = 1;
-			}else{
-				my @samples = `find $$outputpath/$$cleanpath/$$runHash{$run} -name "${runid}*clean*.fastx.fastq.gz"`;
-				my $flagsample = $samples[0];
-				chomp $flagsample;
-				if(!-e $flagsample){
-					open OUT,">$$outputpath/$$pbspath/$runHash{$run}/$jobname\_$runid".".pbs";
-					print OUT <<EOF;
+			close OUT;
+			$marksfastqfilter{$runid} = 1;
+		}else{
+			my @samples = `find $$outputpath/$$cleanpath/$$runHash{$run} -name "${runid}*clean*.fastx.fastq.gz"`;
+			my $flagsample = $samples[0];
+			chomp $flagsample;
+			if(!-e $flagsample){
+				open OUT,">$$outputpath/$$pbspath/$runHash{$run}/$jobname\_$runid".".pbs";
+				print OUT <<EOF;
 #PBS -N $jobname\_$runid
 #PBS -j oe
 #PBS -l nodes=1:ppn=$$threads
@@ -227,20 +226,19 @@ EOF
 cd \$PBS_O_WORKDIR
 date
 for file in `find $$outputpath/$$cleanpath/$$runHash{$run} -name "${runid}*clean*.fastq.gz"`
-	do 
-		sample=\${file%.fastq.gz}
-		sample=\${sample##\/*\/}
-		gunzip -c \$file | fastq_quality_filter -v -q $QUAL -p $PERCENT -z -Q 33 -o $$outputpath/$$cleanpath/$$runHash{$run}/\$sample.fastx.fastq.gz
-	done
+do 
+	sample=\${file%.fastq.gz}
+	sample=\${sample##\/*\/}
+	gunzip -c \$file | fastq_quality_filter -v -q $QUAL -p $PERCENT -z -Q 33 -o $$outputpath/$$cleanpath/$$runHash{$run}/\$sample.fastx.fastq.gz
+done
 fastqc -o $$outputpath/$$fastqcpath/$$runHash{$run} -t $$threads --extract -f fastq $$outputpath/$$cleanpath/$$runHash{$run}/$runid*.fastx.fastq.gz
 date
 EOF
-					close OUT;
-					$marksfastqfilter{$runid} = 1;
-				}else{
-					$marksfastqfilter{$runid} = 0;
-				}	
-			}
+				close OUT;
+				$marksfastqfilter{$runid} = 1;
+			}else{
+				$marksfastqfilter{$runid} = 0;
+			}	
 		}
 	}
 	&submitJob($runHash,$outputpath,$pbspath,$jobname,\%marksfastqfilter,$species,$queue);
